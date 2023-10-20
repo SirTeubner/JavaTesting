a42ac576a2fcd7d2664f9f18687728f0c7e0e809
Noah Teubner <nteubner@schueler.hakzell.at>
2023-10-20 23:50:10 +0200

    Data types "added"

--- QuickStart.java
+++ QuickStart.java
@@ -1,5 +1,8 @@
 class QuickStart{
     public static void main(String[] args){
-        System.out.println("Hello World.");
+        System.out.println("Data types");
+        System.out.println("boolen, true or false \n byte, very small numbers \n short, small numbers \n int, normal long numbers \n long, long numbers \n float, small fractional number  \n double, normal long fractional number\n char, single characters \n string, sequence of characters");
+
+
     }
 }
