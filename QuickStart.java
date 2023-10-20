e2e5fbdb038227790c9f2f90532c649906ddc299
Noah Teubner <nteubner@schueler.hakzell.at>
2023-10-20 23:59:20 +0200

    testing variables

--- QuickStart.java
+++ QuickStart.java
@@ -4,6 +4,22 @@ class QuickStart{
         System.out.println("Data types");
         System.out.println("boolen, true or false \n byte, very small numbers \n short, small numbers \n int, normal long numbers \n long, long numbers \n float, small fractional number  \n double, normal long fractional number\n char, single characters \n string, sequence of characters");
 
+        // int x; declaration
+        // x = 123; assignment
+
+        int x = 123; // initialization
+        long y = 12341251545351315L;
+        boolean z = true;
+        char symbol = '@';
+        String name = "Noah";
+
+        System.out.println("My number is: " + x);
+        System.out.println("My number is not: " + y);
+        System.out.println(z);
+
+        System.out.println(symbol);
+        System.out.println("Hello: " + name);
+        
         
 
 
